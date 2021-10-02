11032178f0426855832dd9a56390d1fbaaebcc2b
Joey Steigelman <joey.steigelman@gmail.com>
2021-10-02 18:47:39 +0200

    changed marquee text to reusable component

--- src/Components/Marquee/Marquee.js
+++ src/Components/Marquee/Marquee.js
@@ -2,15 +2,31 @@ import React from "react";
 import "./Marquee.css";
 import Marquee from "react-fast-marquee";
 
+export class MarqueeText extends React.Component {
+  render() {
+    return (
+      <p>Say hi at joey.steigelman@gmail.com ·{" "}</p>
+    )
+  }
+}
+
 export class MarqueeTicker extends React.Component {
   render() {
     return (
       <div className="marquee">
-        <Marquee gradient={false} speed={90}>
-          <p>
-            Say hi at joey.steigelman@gmail.com · Say hi at
-            joey.steigelman@gmail.com · Say hi at joey.steigelman@gmail.com ·{" "}
-          </p>
+        <Marquee loop={0} gradient={false} speed={90}>
+          <MarqueeText />
+          <MarqueeText />
+          <MarqueeText />
+          <MarqueeText />
+          <MarqueeText />
+          <MarqueeText />
+          <MarqueeText />
+          <MarqueeText />
+          <MarqueeText />
+          <MarqueeText />
+          <MarqueeText />
+          <MarqueeText />
         </Marquee>
       </div>
     );
