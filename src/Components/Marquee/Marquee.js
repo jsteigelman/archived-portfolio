e0537ad9b87b37676585714ab4a84a6371f7ff29
Joey Steigelman <joey.steigelman@gmail.com>
2021-10-09 14:10:11 +0200

    refactored code for Marquee on landing page

--- src/Components/Marquee/Marquee.js
+++ src/Components/Marquee/Marquee.js
@@ -2,37 +2,12 @@ import React from "react";
 import "./Marquee.css";
 import Marquee from "react-fast-marquee";
 
-export class MarqueeText extends React.Component {
-  render() {
-    return (
-      <p>Say hi at joey.steigelman@gmail.com ·{" "}</p>
-    )
-  }
-}
-
 export class MarqueeTicker extends React.Component {
   render() {
     return (
-      <div className="home-marquee">
-        <Marquee loop={0} gradient={false} speed={90} >
-          <MarqueeText />
-          <MarqueeText />
-          <MarqueeText />
-          <MarqueeText />
-          <MarqueeText />
-          <MarqueeText />
-          <MarqueeText />
-          <MarqueeText />
-          <MarqueeText />
-          <MarqueeText />
-          <MarqueeText />
-          <MarqueeText />
-          <MarqueeText />
-          <MarqueeText />
-          <MarqueeText />
-          <MarqueeText />
+        <Marquee className="home-marquee" loop={0} gradient={false} speed={90} >
+          <h1>Say hi at joey.steigelman@gmail.com · Say hi at joey.steigelman@gmail.com · Say hi at joey.steigelman@gmail.com · Say hi at joey.steigelman@gmail.com · Say hi at joey.steigelman@gmail.com · Say hi at joey.steigelman@gmail.com · Say hi at joey.steigelman@gmail.com ·{" "}</h1>
         </Marquee>
-      </div>
     );
   }
 }
