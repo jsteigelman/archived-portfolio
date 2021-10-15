266579a05e55fd8e42a8ba57d5460b04155822a9
Joey Steigelman <joey.steigelman@gmail.com>
2021-10-15 17:12:49 +0200

    updated swiper and cards ui

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { Intro } from "./../Intro/Intro.js";
 import { Cards } from "./../Cards/Cards.js";
+import { CardSwiper } from "./../Cards/CardSwiper.js";
+
 import { Project, ProjectVideo } from "./../Projects/Projects.js";
 import { MarqueeTicker } from "./../Marquee/Marquee.js";
 import ReactGA from "react-ga";
@@ -9,13 +11,15 @@ ReactGA.initialize("UA-203154360-2");
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
+
+  
   return (
     <div className="App" id="contact">
       <Intro />
       <MarqueeTicker />
       <span id="skills" ></span>
-
       <Cards />
+      <CardSwiper />
       <div
         style={{
           borderTop: "1px solid #E3E3E3 ",
